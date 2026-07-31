2a84bfb4cff4b8cfd4b6e06c00fcd0fd14ddc248
Sorbo1 <sorbo455@gmail.com>
2026-07-31 08:06:04 +0200

    Update deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,3 +1,43 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-console.log("Sem přijde registrace Slash Commands.");
+import { REST, Routes } from "discord.js";
+
+import setup from "./commands/setup.js";
+
+const commands=[
+
+    setup.data.toJSON()
+
+];
+
+const rest=new REST({version:"10"}).setToken(process.env.TOKEN);
+
+(async()=>{
+
+    try{
+
+        console.log("Registruji Slash Commands...");
+
+        await rest.put(
+
+            Routes.applicationGuildCommands(
+
+                process.env.CLIENT_ID,
+
+                process.env.GUILD_ID
+
+            ),
+
+            {body:commands}
+
+        );
+
+        console.log("Hotovo.");
+
+    }catch(err){
+
+        console.error(err);
+
+    }
+
+})();
